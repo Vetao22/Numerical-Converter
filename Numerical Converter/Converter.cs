f19c7068e5cc9fa479288a50f9c9764a3de8eea1
Everton da Costa Duarte <zecsharp@hotmail.com>
2020-03-10 22:31:25 -0300

    Adicionar arquivos de projeto.

---
+++ Numerical Converter/Converter.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numerical_Converter
+{
+    public class Converter
+    {
+        public static string DecToBin(string input)
+        {
+            string result = "";
+
+            int value = Convert.ToInt32(input);
+            int quotient, remain;
+
+            quotient = value;
+
+            do
+            {
+                remain = (int)(quotient % 2);
+                quotient = (int)(quotient / 2);
+
+                result = remain + result;
+            }
+            while (quotient != 0);
+
+            return result;
+        }
+
+        public static string BinToDec(string input)
+        {
+            int result = 0;
+            int power = input.Length - 1;
+
+            for(int x = 0; x < input.Length; x++)
+            {
+                int value = (int)Char.GetNumericValue(input.ElementAt(x));
+                result += (value * (int)Math.Pow(2, power));
+                power--;
+            }
+
+            return result.ToString();
+        }
+
+        public static string DecToHex(string input)
+        {
+            string result = "";
+
+            int value = Convert.ToInt32(input);
+            int quotient, remain;
+
+            quotient = value;
+
+            do
+            {
+                remain = (int)(quotient % 16);
+                quotient = (int)(quotient / 16);
+
+                if(remain > 9 && remain < 16)
+                {
+                    string temp = "";
+
+                    switch(remain)
+                    {
+                        case 10:
+                            temp = "A";
+                            break;
+
+                        case 11:
+                            temp = "B";
+                            break;
+
+                        case 12:
+                            temp = "C";
+                            break;
+
+                        case 13:
+                            temp = "D";
+                            break;
+
+                        case 14:
+                            temp = "E";
+                            break;
+
+                        case 15:
+                            temp = "F";
+                            break;
+                    }
+
+                    result = temp + result;
+                }
+                else
+                {
+                    result = remain + result;
+                }
+
+               
+            }
+            while (quotient != 0);
+
+            return result;
+        }
+
+        public static string HexToDec(string input)
+        {
+            input = input.ToUpper();
+            int result = 0;
+            int power = input.Length - 1;
+
+            for (int x = 0; x < input.Length; x++)
+            {
+                char curChar = input[x];
+                int value = 0;
+
+                if (!Char.IsNumber(curChar))
+                {
+                    switch (curChar)
+                    {
+                        case 'A':
+                            value = 10;
+                            break;
+
+                        case 'B':
+                            value = 11;
+                            break;
+
+                        case 'C':
+                            value = 12;
+                            break;
+
+                        case 'D':
+                            value = 13;
+                            break;
+
+                        case 'E':
+                            value = 14;
+                            break;
+
+                        case 'F':
+                            value = 15;
+                            break;
+
+                        default:
+                            return "";                            
+                    }
+
+                    result += (int)(value * Math.Pow(16, power));
+                }
+                else
+                {
+                    value = (int)Char.GetNumericValue(input.ElementAt(x));
+                    result += (int)(value * Math.Pow(16, power));
+                }
+                
+                power--;
+            }
+
+            return result.ToString();
+        }
+
+        public static string HexToBin(string input)
+        {
+            input = input.ToUpper();
+
+            string result = HexToDec(input);
+            result = DecToBin(result);
+
+            return result;
+        }
+
+        public static string BinToHex(string input)
+        {
+            input = input.ToUpper();
+
+            string result = BinToDec(input);
+            result = DecToHex(result);
+
+            return result;
+        }
+    }
+}
---
+++ Numerical Converter/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Numerical_Converter
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string text = txtInput.Text;
+
+            int convertionSelected = cmbChoice.SelectedIndex;
+
+            if(convertionSelected == 0 || convertionSelected == 2)
+            {
+                txtInput.Text = RemoveLetters(text);
+            }            
+            MaxDigitsReached();
+
+            txtInput.CaretIndex = txtInput.Text.Count();
+
+        }
+
+        string RemoveLetters(string text)
+        {
+            string result = "";
+
+            foreach(char c in text)
+            {
+                if(Char.IsNumber(c))
+                {
+                    result += c;
+                }
+            }
+
+            return result;
+        }
+
+        void MaxDigitsReached()
+        {
+            if(txtInput.Text.Count() == txtInput.MaxLength)
+            {
+                MessageBox.Show("You can type up to 8 digits", "Atention!", MessageBoxButton.OK, MessageBoxImage.Exclamation );
+            }
+        }
+
+        private void btnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            if(!String.IsNullOrEmpty(txtInput.Text))
+            {
+                int convertionSelected = cmbChoice.SelectedIndex;
+                string input = txtInput.Text;
+
+                switch(convertionSelected)
+                {
+                    case 0:
+                        txtResult.Text = Converter.DecToBin(input);
+                        break;
+
+                    case 1:
+                        txtResult.Text = Converter.BinToDec(input);
+                        break;
+
+                    case 2:
+                        txtResult.Text = Converter.DecToHex(input);
+                        break;
+
+                    case 3:
+                        txtResult.Text = Converter.HexToDec(input);
+                        break;
+
+                    case 4:
+                        txtResult.Text = Converter.BinToHex(input);
+                        break;
+
+                    case 5:
+                        txtResult.Text = Converter.HexToBin(input);
+                        break;
+                }
+                
+            }
+            
+        }
+
+        private void cmbChoice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (txtInput != null)
+            {
+                txtInput.Text = "";
+            }
+        }
+    }
+}
